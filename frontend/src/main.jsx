c3808bd7f71bed0c72506f5045154b25be59cdfd
eniya <eniya0516@gmail.com>
2025-12-19 14:20:14 +0530

    Vite build success - ready for Vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-// Import Redux Provider and Store
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>  {/* <-- Add this wrapper */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>  {/* <-- Close wrapper here */}
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
